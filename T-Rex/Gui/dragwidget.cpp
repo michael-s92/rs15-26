29901e1cad05e67e5afa2dd583a4826317ded5e7
jelenamats1 <jelenapajic7@gmail.com>
2015-05-21 12:25:53 +0200

    popravka dijagrama

--- T-Rex/Gui/dragwidget.cpp
+++ T-Rex/Gui/dragwidget.cpp
@@ -17,37 +17,103 @@ DragWidget::DragWidget(QWidget *parent)
 
     QPushButton *tmp;
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag1.ico", "slovo", 40, false);
-    tmp->setText("Slovo");
-    layout->addWidget(tmp);
+    QImage image(":/toolBox/images/diag1.ico");
+    QPainter* painter1 = new QPainter(&image);
+    painter1->setPen(Qt::blue);
+    painter1->setFont(QFont("Arial", 30));
+    painter1->drawText(image.rect(), Qt::AlignBaseline, "slovo");
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag2.ico", "konkatenacija", 40, false);
-    tmp->setText("Nadovezi");
-    layout->addWidget(tmp);
+    QLabel* imageLabel = new QLabel();
+    imageLabel->setPixmap(QPixmap::fromImage(image));
+    imageLabel->setAlignment(Qt::AlignCenter);
+    imageLabel->setToolTip("slovo");
+    layout->addWidget(imageLabel);
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag3.ico", "disjunkcija", 40, false);
-    tmp->setText("ili");
-    layout->addWidget(tmp);
+    QImage image1(":/toolBox/images/diag2.ico");
+    QPainter* painter2 = new QPainter(&image1);
+    painter2->setPen(Qt::blue);
+    painter2->setFont(QFont("Arial", 30));
+    painter2->drawText(image.rect(), Qt::AlignBaseline, "Nadovezi");
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag4.ico", "jednom ili ni jednom prikazi", 40, false);
-    tmp->setText("Jednom ili nijednom");
-    layout->addWidget(tmp);
+    QLabel* imageLabel1 = new QLabel();
+    imageLabel1->setPixmap(QPixmap::fromImage(image1));
+    imageLabel1->setAlignment(Qt::AlignCenter);
+    imageLabel1->setToolTip("konkatenacija");
+    layout->addWidget(imageLabel1);
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag5.ico", "jedan ili vise puta", 40, false);
-    tmp->setText("Jedan ili vise");
-    layout->addWidget(tmp);
+    QImage image2(":/toolBox/images/diag3.ico");
+    QPainter* painter3 = new QPainter(&image2);
+    painter3->setPen(Qt::blue);
+    painter3->setFont(QFont("Arial", 30));
+    painter3->drawText(image.rect(), Qt::AlignBaseline, "ili");
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag6.ico", "nula ili vise puta prikazi", 40, false);
-    tmp->setText("Nula ili vise");
-    layout->addWidget(tmp);
+    QLabel* imageLabel2 = new QLabel();
+    imageLabel2->setPixmap(QPixmap::fromImage(image2));
+    imageLabel2->setAlignment(Qt::AlignCenter);
+    imageLabel2->setToolTip("disjunkcija");
+    layout->addWidget(imageLabel2);
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag7.ico", "Tacno toliko", 40, false);
-    tmp->setText("Tacno toliko puta");
-    layout->addWidget(tmp);
+    QImage image3(":/toolBox/images/diag4.ico");
+    QPainter* painter4 = new QPainter(&image3);
+    painter4->setPen(Qt::blue);
+    painter4->setFont(QFont("Arial", 30));
+    painter4->drawText(image.rect(), Qt::AlignBaseline, "Jednom ili nijednom");
 
-    tmp = GuiBuilder::createIconButton(":/toolBox/images/diag8.ico", "Karakterna klasa", 40, false);
-    tmp->setText("Jedno od");
-    layout->addWidget(tmp);
+    QLabel* imageLabel3 = new QLabel();
+    imageLabel3->setPixmap(QPixmap::fromImage(image3));
+    imageLabel3->setAlignment(Qt::AlignCenter);
+    imageLabel3->setToolTip("jednom ili ni jednom prikazi");
+    layout->addWidget(imageLabel3);
+
+    QImage image4(":/toolBox/images/diag5.ico");
+    QPainter* painter5 = new QPainter(&image4);
+    painter5->setPen(Qt::blue);
+    painter5->setFont(QFont("Arial", 30));
+    painter5->drawText(image.rect(), Qt::AlignBaseline, "jedan ili vise");
+
+    QLabel* imageLabel4 = new QLabel();
+    imageLabel4->setPixmap(QPixmap::fromImage(image4));
+    imageLabel4->setAlignment(Qt::AlignCenter);
+    imageLabel4->setToolTip("jedan ili vise puta");
+    layout->addWidget(imageLabel4);
+
+    QImage image6(":/toolBox/images/diag6.ico");
+    QPainter* painter6 = new QPainter(&image6);
+    painter6->setPen(Qt::blue);
+    painter6->setFont(QFont("Arial", 30));
+    painter6->drawText(image.rect(), Qt::AlignBaseline, "Nula ili vise");
+
+    QLabel* imageLabel6 = new QLabel();
+    imageLabel6->setPixmap(QPixmap::fromImage(image6));
+    imageLabel6->setAlignment(Qt::AlignCenter);
+    imageLabel6->setToolTip("nula ili vise puta prikazi");
+    layout->addWidget(imageLabel6);
+
+    QImage image7(":/toolBox/images/diag7.ico");
+    QPainter* painter7 = new QPainter(&image7);
+    painter7->setPen(Qt::blue);
+    painter7->setFont(QFont("Arial", 30));
+    painter7->drawText(image.rect(), Qt::AlignBaseline, "Tacno toliko puta");
+
+    QLabel* imageLabel7 = new QLabel();
+    imageLabel7->setPixmap(QPixmap::fromImage(image7));
+    imageLabel7->setAlignment(Qt::AlignCenter);
+    imageLabel7->setToolTip("Tacno toliko");
+    layout->addWidget(imageLabel7);
+
+    QImage image8(":/toolBox/images/diag8.ico");
+    QPainter* painter8 = new QPainter(&image8);
+    painter8->setPen(Qt::blue);
+    painter8->setFont(QFont("Arial", 30));
+    painter8->drawText(image.rect(), Qt::AlignBaseline, "jedno od");
+
+    QLabel* imageLabel8 = new QLabel();
+    imageLabel8->setPixmap(QPixmap::fromImage(image8));
+    imageLabel8->setAlignment(Qt::AlignCenter);
+    imageLabel8->setToolTip("Karakterska klasa");
+    layout->addWidget(imageLabel8);
+
+
 
     /*
     QLabel *boatIcon = new QLabel(this);
